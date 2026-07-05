3621010fae40821e4b3280de77da02db10d25585
Endy® <enzoborrellidev@gmail.com>
2026-07-05 12:47:54 -0300

    migrate to create REGISTRATE

--- src/main/java/com/endykaishi/createatm/CreateAllthemodium.java
+++ src/main/java/com/endykaishi/createatm/CreateAllthemodium.java
@@ -1,67 +1,50 @@
 package com.endykaishi.createatm;
 
-import org.slf4j.Logger;
-
+import com.endykaishi.createatm.registry.ModBlocks;
+import com.endykaishi.createatm.registry.ModItems;
+import com.endykaishi.createatm.visual.ModSpriteShifts;
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreateAllthemodium.MOD_ID)
 public class CreateAllthemodium {
-    // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "create_allthemodium";
-    // Directly reference a slf4j logger
-    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public CreateAllthemodium(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-        NeoForge.EVENT_BUS.register(this);
+	// Define mod id in a common place for everything to reference
+	public static final String MOD_ID = "create_allthemodium";
+	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(CreateAllthemodium.MOD_ID);
+	// Directly reference a slf4j logger
+	public static final Logger LOGGER = LogUtils.getLogger();
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+	public CreateAllthemodium(IEventBus modEventBus, ModContainer modContainer) {
+		// Register the commonSetup method for modloading
+		REGISTRATE.registerEventListeners(modEventBus);
+		modEventBus.addListener(this::commonSetup);
+		modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
+		NeoForge.EVENT_BUS.register(this);
 
-    private void commonSetup(FMLCommonSetupEvent event) {
+		// Register our mod's ModConfigSpec so that FML can create and load the config file for us
+		modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+		ModSpriteShifts.init();
+	}
 
-    }
+	private void commonSetup(FMLCommonSetupEvent event) {}
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+	// Add the example block item to the building blocks tab
+	private void addCreative(BuildCreativeModeTabContentsEvent event) {}
 
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event) {}
 }
